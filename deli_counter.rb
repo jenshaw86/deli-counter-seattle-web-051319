ee854bbf82bf6a0737add86ebbcd60df3c459321
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-06 04:05:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,11 @@ def line(queue)
   if queue.length < 1
     puts "The line is currently empty."
   else
-    line_up = "The line is currently: "
+    count = 0
+    line_up = "The line is currently:"
     queue.each do |customer|
+      count += 1
+      " #{count}. #{customer}"
       
   end
 end
