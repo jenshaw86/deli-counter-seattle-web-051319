68cf209fce82b6e6066566db62ea8ef6c831b45c
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-06 04:09:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,18 @@
 def line(queue)
-  count = 0
+  count = 1
   if queue.length < 1
     puts "The line is currently empty."
   else
     line_up = "The line is currently:"
     queue.each do |customer|
-      count += 1
       next_in_line = " #{count}. #{customer}"
       line_up.concat(next_in_line)
+      count += 1
     end
     puts line_up
   end
 end
 
-def take_a_number
+def take_a_number(customer)
+  count = 1
   
